4c63a321095b73ae14b32f30347dc36007c8aa3d
sAksham-Ar <57193459+sAksham-Ar@users.noreply.github.com>
2020-12-08 16:06:30 +0530

    Added a lot of languages

--- codeify.js
+++ codeify.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
  const client = new Discord.Client();
 // require the highlight.js library, including all languages
 const hljs = require("highlight.js");  // require only the core library
+
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
@@ -9,8 +10,8 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.author.bot) return;
 
- if(hljs.highlightAuto(msg.content,["cpp","py","js"]).language ){
- msg.reply("```"+hljs.highlightAuto(msg.content,["cpp","py","js"]).language+'\n'+msg.content+"```");
+ if(hljs.highlightAuto(msg.content,["cpp","py","js","autohotkey","c","cs","css","go","java","json","tex","matlab","php","ps","bash"]).language ){
+ msg.reply("```"+hljs.highlightAuto(msg.content,["cpp","py","js","autohotkey","c","cs","css","go","java","json","tex","matlab","php","ps","bash"]).language+'\n'+msg.content+"```");
  }
  });
 
